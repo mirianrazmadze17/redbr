1cd1211fae88376c51180dd6ef77bbe386995886
hideo <hk6@gmail.com>
2022-07-10 10:53:30 +0400

    new commit

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 let dropdown = document.querySelector(".newDiv");
 let optionClick = document.getElementById("character");
 let option = document.getElementById("opt");
-
+let submit1 = document.getElementById("submit1");
 let mastersDiv = document.querySelector(".masters-div");
+let nameValue = document.getElementById("name").value.toString().length;
+console.log(nameValue);
+console.log(submit1);
+let emailInput = document.getElementById("email");
+let numberInput = document.getElementById("number");
+let dateInput = document.getElementById("date");
+console.log(nameValue.length);
+submit1.addEventListener("click", (e) => {
+  e.preventDefault;
+  if (
+    $("#name").val().length &&
+    emailInput.value.substr(-12) == "@redberry.ge" &&
+    numberInput.value.length == 9
+  ) {
+    window.location.href = "./register2.html";
+  }
+});
 $("#character").click(() => {
   $(".masters-div").toggleClass("active");
 });
@@ -10,32 +27,32 @@ $("#done").click(() => {
   window.location.href = "./thanks.html";
 });
 // get img data
-let data = async () => {
-  try {
-    let data = await fetch(
-      "https://chess-tournament-api.devtest.ge/api/grandmasters"
-    ).then((res) => res.json());
-    console.log(data);
-    let images = data.forEach((el) => {
-      let spans = el.name;
-      let imgs = el.image;
-      let span = document.createElement("span");
-      let img = document.createElement("img");
-      img.src = `https://chess-tournament-api.devtest.ge/${imgs}`;
-      img.classList.add("masters");
-      span.innerHTML = spans;
-      let newDiv = document.createElement("div");
-      newDiv.classList.add("masters-div");
-      newDiv.appendChild(span);
-      newDiv.appendChild(img);
-      dropdown.append(newDiv);
-      $(span).click(() => {
-        option.innerHTML = spans;
-        option.value = spans;
-      });
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-data();
+// let data = async () => {
+//   try {
+//     let data = await fetch(
+//       "https://chess-tournament-api.devtest.ge/api/grandmasters"
+//     ).then((res) => res.json());
+//     console.log(data);
+//     let images = data.forEach((el) => {
+//       let spans = el.name;
+//       let imgs = el.image;
+//       let span = document.createElement("span");
+//       let img = document.createElement("img");
+//       img.src = `https://chess-tournament-api.devtest.ge/${imgs}`;
+//       img.classList.add("masters");
+//       span.innerHTML = spans;
+//       let newDiv = document.createElement("div");
+//       newDiv.classList.add("masters-div");
+//       newDiv.appendChild(span);
+//       newDiv.appendChild(img);
+//       dropdown.append(newDiv);
+//       $(span).click(() => {
+//         option.innerHTML = spans;
+//         option.value = spans;
+//       });
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// data();
